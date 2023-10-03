ade417995bc2a275b5aed19bf6e13bc0c69c93db
Adriele Primo <adriele.dejprimo@gmail.com>
2023-10-02 21:38:03 -0300

    Organizando componente Header

--- my-app/src/componentes/Header.js
+++ my-app/src/componentes/Header.js
@@ -2,19 +2,18 @@
 // Ogranização do header para pesquisa das receitas e título do site
 export default function Header() {
     return (
-      
       <header className="header">
         <div className="pesquisar">
         <nav>
           <ul>
-            <li className="inicio"><a href="/">Início</a></li>
-            <li ><a href="/receitas" >Pesquisar Receitas</a></li>
-            <li ><a href="/receitas-por-letra">Receitas por Letra</a></li>
-            <li ><a href="/receitas-por-ingredientes">Receitas por Ingredientes</a></li>
+            <li className="inicio"><a href="/" className="link">Início</a></li>
+            <li className="abas-1"><a href="/receitas"className="link" >Pesquisar Receitas</a></li>
+            <li><a href="/receitas-por-letra" className="link">Receitas por Letra</a></li>
+            <li><a href="/receitas-por-ingredientes" className="link">Receitas por Ingredientes</a></li>
             
           </ul>
         </nav>
-        <h1>Receitas da Dri</h1>
+        <h1>Receitas da Meal</h1>
         </div>
       </header>
     )
